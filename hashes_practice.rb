84730bb12fff18d97aed7354fdb0138db330a0ed
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-10-22 13:40:52 -0600

    Complete hash syntax practice pt.1

---
+++ hashes_practice.rb
@@ -0,0 +1,14 @@
+# Complete the following work in an irb session or a new Ruby file, working in VS Code:
+
+# For one of the examples in the previous activity that you selected would be best suited for a Hash, declare a variable that stores a Hash with some (possibly fake) data.
+# Declare a variable that stores a Hash that represents this tweet.
+
+s_birthdays = {
+    Franco_Varelli: "06/20/1992",
+    Jessie_Lambda: "03/05/1994",
+    Vick_De_Jesus: "12/16/1978",
+    Mark_Harrington: "10/23/1989",
+    John_Freid: "11/11/1997",
+    Justina_Pope: "01/01/2005",
+    Elizabeth_Mahon: "07/15/1983",
+}
